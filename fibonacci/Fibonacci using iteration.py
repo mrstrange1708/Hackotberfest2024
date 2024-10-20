f18ca4adf0c3dea51b97078c417ae70834d08d27
Shaik Mohammed Junaid Sami <junaidsamishaik@gmail.com>
2024-10-20 16:02:32 +0530

    Update Fibonacci using iteration.py
    The easy way to do this

--- fibonacci/Fibonacci using iteration.py
+++ fibonacci/Fibonacci using iteration.py
@@ -1,18 +1,10 @@
-# Find Fibonacci Series in Python Using While Loop
-# we are taking a number from user as input
-
-# entered value will be converted to int from string
 n = int(input("Number of Fibonacci Series to be Printed: "))
 
-# print initial 2 number to start fibonacci series
-a, b = 0, 1
-print(a, b, end=' ')
+a = 0 
+b = 1
 
-i = 0
-while (i < n-2):
-    c = a + b
-    # add the last two numbers and print
-    print(c, end=' ')
+for i in range(1,n+1):
+    print(a,end=" ")
+    c = a+b
     a = b
     b = c
-    i = i + 1
